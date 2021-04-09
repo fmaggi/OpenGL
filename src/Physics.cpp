ae17f78ce9c1c63b046fd8b4dd8b13b850ba8eb6
fmaggi <franciscomaggi1999@gmail.com>
2021-04-09 13:51:31 -0300

    Physics Module

---
+++ src/Physics.cpp
@@ -0,0 +1,49 @@
+#include "Physics.h"
+
+#include "glm/glm.hpp"
+#include <glm/gtx/matrix_decompose.hpp>
+
+
+namespace Physics
+{
+	const static float GRAVITY = -9.8f;
+	const static float FLOOR = -4.0f;
+
+	void update(Entity* ntt, float ts)
+	{
+		glm::vec3 m_pos = ntt->getPosition();
+		glm::vec2 m_speed = ntt->getSpeed();
+
+		glm::mat4 trans = ntt->getTransformationMatrix();
+		glm::vec3 scale;
+		glm::quat rotation;
+		glm::vec3 translation;
+		glm::vec3 skew;
+		glm::vec4 perspective;
+		glm::decompose(trans, scale, rotation, translation, skew, perspective);
+
+		float cosZ = glm::cos(rotation.z);
+		float sinZ = glm::sin(rotation.z);
+
+		float normal = GRAVITY * -1 * (m_pos.y == FLOOR);
+		float acceleration_y = GRAVITY + (ntt->getPower() * cosZ)/ntt->getMass() + normal;
+		float acceleration_x = (ntt->getPower() * sinZ) / ntt->getMass();
+
+		float speed_y = m_speed.y + acceleration_y * ts;
+		float speed_x = m_speed.x + acceleration_x * ts;
+
+		float move_y = m_speed.y * ts + 0.5 * acceleration_y * ts * ts;
+		float move_x = m_speed.x * ts + 0.5 * acceleration_x * ts * ts;
+
+		if (m_pos.y + move_y <= FLOOR)
+		{
+			ntt->setPosition(glm::vec3(m_pos.x, FLOOR, m_pos.z));
+			ntt->setSpeed(0, 0);
+			return;
+		}
+
+		ntt->move(glm::vec3(-move_x, move_y, 0.0f));
+
+		ntt->setSpeed(speed_x, speed_y);
+	}
+}
---
+++ src/Physics.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "Entity.h"
+
+namespace Physics
+{
+	void update(Entity* ntt, float ts);
+}
